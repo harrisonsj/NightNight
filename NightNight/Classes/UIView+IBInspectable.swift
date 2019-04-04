e76d8a9eadde1e36ccd6deb47b781ce71403e300
Samuel Harrison <samuel.harrison@me.com>
2019-04-04 17:27:37 +0100

    More IBInspectable values

--- NightNight/Classes/UIView+IBInspectable.swift
+++ NightNight/Classes/UIView+IBInspectable.swift
@@ -14,7 +14,7 @@ public extension UIView {
         set {
             objc_setAssociatedObject(self, &NightKeys.backgroundColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             if let mixedColor = MixedColor(normalBackgroundColor, night: newValue){
-                objc_setAssociatedObject(self, &Keys.backgroundColor, mixedColor, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                mixedBackgroundColor = mixedColor
             }
         }
     }
@@ -23,7 +23,7 @@ public extension UIView {
         set {
             objc_setAssociatedObject(self, &NormalKeys.backgroundColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             if let mixedColor = MixedColor(newValue, night: nightBackgroundColor){
-                objc_setAssociatedObject(self, &Keys.backgroundColor, mixedColor, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                mixedBackgroundColor = mixedColor
             }
         }
     }
@@ -32,12 +32,146 @@ public extension UIView {
         get { return objc_getAssociatedObject(self, &NightKeys.tintColor) as? UIColor }
         set {
             objc_setAssociatedObject(self, &NightKeys.tintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTintColor, night: newValue){
+                mixedTintColor = mixedColor
+            }
         }
     }
     @IBInspectable public var normalTintColor: UIColor? {
         get { return objc_getAssociatedObject(self, &NormalKeys.tintColor) as? UIColor }
         set {
             objc_setAssociatedObject(self, &NormalKeys.tintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTintColor){
+                mixedTintColor = mixedColor
+            }
+        }
+    }
+}
+
+public extension UILabel{
+    @IBInspectable public var nightTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTextColor, night: newValue){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTextColor){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+}
+
+extension UIBarButtonItem{
+    @IBInspectable public var nightTintColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.tintColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.tintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTintColor, night: newValue){
+                //mixedTintColor = mixedColor
+                //mixedColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalTintColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.tintColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.tintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTintColor){
+                //mixedTintColor = mixedColor
+                //mixedColor = mixedColor
+            }
+        }
+    }
+}
+
+extension UINavigationBar{
+    @IBInspectable public var nightBarTintColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.barTintColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.barTintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalBarTintColor, night: newValue){
+                mixedBarTintColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalBarTintColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.barTintColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.barTintColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightBarTintColor){
+                mixedBarTintColor = mixedColor
+            }
+        }
+    }
+}
+
+public extension UITextField{
+    @IBInspectable public var nightTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTextColor, night: newValue){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTextColor){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+}
+
+public extension UITextView{
+    @IBInspectable public var nightTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTextColor, night: newValue){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTextColor){
+                mixedTextColor = mixedColor
+            }
+        }
+    }
+}
+
+public extension UIButton{
+    @IBInspectable public var nightTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(normalTextColor, night: newValue){
+                titleLabel?.mixedTextColor = mixedColor
+            }
+        }
+    }
+    @IBInspectable public var normalTextColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NormalKeys.textColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NormalKeys.textColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            if let mixedColor = MixedColor(newValue, night: nightTextColor){
+                titleLabel?.mixedTextColor = mixedColor
+            }
         }
     }
 }
